292da51c48c915b919d750bbda8cabb6d1beb870
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-11-20 11:19:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!'
+const prefix = '-'
 const adminprefix = "-";
 const developers = ['505176071159939072'];
 
