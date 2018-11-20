ab70dd07dd7e2eb83611a8c9b349da5118163002
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-11-20 15:24:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '-'
 const adminprefix = "-";
-const developers = ['505176071159939072'];
+const developers = ['412275773014016011'];
 
 client.on('ready', () => {
  console.log("hi"); 
@@ -30,12 +30,6 @@ client.on('message', message => {
   } else
   if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
-  }
-  if (message.content.startsWith(adminprefix + 'sn')) {
-  client.user.setUsername(argresult).then
-} else
-if (message.content.startsWith(adminprefix + 'sa')) {
-  client.user.setAvatar(argresult);
 }
 });
 client.login(process.env.TOKEN);
